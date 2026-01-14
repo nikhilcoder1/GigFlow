d8dc3bd01cc67e6fe3b2c2c7f93a31b088e7d57c
dev_nick <developernikhil28@gmail.com>
2026-01-14 22:26:36 +0530

    Backend Issue

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -68,7 +68,7 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
@@ -90,7 +90,8 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
   });
 
   res.json({ message: "Logged out successfully" });
