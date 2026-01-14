d9e597cf111d935d503f9f9e2f110948c925fb3d
dev_nick <developernikhil28@gmail.com>
2026-01-14 15:26:50 +0530

    Initial Backend Setup

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error(err));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
