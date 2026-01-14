9be64376880f7b780ec4f42a0d6b9cc6e8c2c6c7
dev_nick <developernikhil28@gmail.com>
2026-01-14 17:22:02 +0530

    Backend ready for deployment

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -68,8 +68,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,        // set true in production (HTTPS)
-      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
